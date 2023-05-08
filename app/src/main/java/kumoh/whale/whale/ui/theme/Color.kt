dd48670df4a505bc7f1bfcce020b37f3862e2990
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-05-08 15:39:34 +0900

    app main color 설정

--- app/src/main/java/kumoh/whale/whale/ui/theme/Color.kt
+++ app/src/main/java/kumoh/whale/whale/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package kumoh.whale.whale.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val mainBlue = Color(0xFF2192FF)
+val skyBlue = Color(0xFFD0E7FF)
+val lightYellow = Color(0xFFFFF3AE)
+val lightPurple = Color(0xFFE8E8FC)
