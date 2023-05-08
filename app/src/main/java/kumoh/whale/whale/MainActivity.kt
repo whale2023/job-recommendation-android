a3e109c74caaeb503c0f2c15f8fdd2d6807c7794
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-05-08 15:07:25 +0900

    [2023.05.08] gradle setting 변경

--- app/src/main/java/kumoh/whale/whale/MainActivity.kt
+++ app/src/main/java/kumoh/whale/whale/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,10 +17,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WhaleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
 
                 }
@@ -30,9 +29,10 @@ class MainActivity : ComponentActivity() {
 }
 
 
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun GreetingPreview() {
     WhaleTheme {
 
     }
