42d721e9d6308daca1c75696eaf551f0f91d487c
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-05-18 18:11:21 +0900

    [2023.05.18] (di) module 생성

---
+++ app/src/main/java/kumoh/whale/whale/di/DataModule.kt
@@ -0,0 +1,11 @@
+package kumoh.whale.whale.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DataModule {
+}
---
+++ app/src/main/java/kumoh/whale/whale/di/PresentationModule.kt
@@ -0,0 +1,11 @@
+package kumoh.whale.whale.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface PresentationModule {
+}
