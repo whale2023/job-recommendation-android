dd3fb268840583dd1f501389ffe94cf30a8f6aa9
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-05-19 19:27:05 +0900

    [2023.05.19] SingUp Screen 생성

---
+++ presentation/src/main/java/kgb/plum/presentation/ui/components/SignUpScreen.kt
@@ -0,0 +1,9 @@
+package kgb.plum.presentation.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignUpScreen(){
+    Text(text = "SIGNUP")
+}
