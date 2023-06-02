690665a3c88f341b269eceb794a39ff2b6511f34
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-06-02 16:33:20 +0900

    위시리스트 리스트 부분 UI 완성

--- presentation/src/main/java/kgb/plum/presentation/ui/components/WishListScreen.kt
+++ presentation/src/main/java/kgb/plum/presentation/ui/components/WishListScreen.kt
@@ -1,22 +1,36 @@
 package kgb.plum.presentation.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kgb.plum.presentation.R
 import kgb.plum.presentation.ui.common.RecommendItem
+import kgb.plum.presentation.ui.common.WishItem
 import kgb.plum.presentation.ui.theme.Padding
 import kgb.plum.presentation.ui.theme.WhaleTheme
 import kgb.plum.presentation.ui.theme.colors
@@ -35,6 +49,30 @@ fun WishListScreen() {
             text = "위시리스트",
             style = MaterialTheme.typography.headlineSmall
         )
+        Spacer(modifier = Modifier.size(Padding.large))
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            itemsIndexed(viewModel.wishList) { index, item ->
+                WishItem(color = if(index%2==0) MaterialTheme.colors.background else MaterialTheme.colors.secondary, company = item.company, occupation = item.occupation, dDay = item.dDay)
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Row {
+            Spacer(modifier = Modifier.weight(1f))
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                shape = CircleShape,
+                containerColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.surface,
+                elevation = FloatingActionButtonDefaults.elevation(10.dp),
+                modifier = Modifier.size(80.dp),
+            ) {
+                Image(painter = painterResource(id = R.drawable.calendar), contentDescription = "달력", modifier = Modifier.fillMaxSize(0.8f))
+            }
+        }
+        Spacer(modifier = Modifier.size(100.dp))
     }
 }
 
